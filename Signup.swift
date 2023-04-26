ea2edc5b1de1e7ac9d59029e7ecc4fae983b875b
Ali Ibrahim <alialchi07@gmail.com>
2023-04-26 14:54:51 -0500

    Finishing Signup Page

--- Signup.swift
+++ Signup.swift
@@ -16,13 +16,13 @@ struct Signup: View {
         VStack {
             Text("Sign Up")
                 .font(.system(size: 50))
-            Divider()
             
             VStack {
                 Text("Username")
                     .font(.system(size: 30))
                 TextField("Enter Username", text: $username)
-                    .font(.system(size: 20))
+                    .font(.system(size: 40))
+                    .frame(width: 800)
                     .textFieldStyle(.roundedBorder)
             }
                 .padding()
@@ -32,7 +32,8 @@ struct Signup: View {
                     .font(.system(size: 30))
                 
                 TextField("Enter Full Name", text: $fullName)
-                    .font(.system(size: 20))
+                    .font(.system(size: 40))
+                    .frame(width: 800)
                     .textFieldStyle(.roundedBorder)
             }
                 .padding()
@@ -42,7 +43,8 @@ struct Signup: View {
                     .font(.system(size: 30))
                 
                 TextField("Enter Email", text: $email)
-                    .font(.system(size: 20))
+                    .font(.system(size: 40))
+                    .frame(width: 800)
                     .textFieldStyle(.roundedBorder)
             }
                 .padding()
@@ -52,7 +54,8 @@ struct Signup: View {
                     .font(.system(size: 30))
                 
                 SecureField("Enter Password", text: $password)
-                    .font(.system(size: 20))
+                    .font(.system(size: 40))
+                    .frame(width: 800)
                     .textFieldStyle(.roundedBorder)
             }
             .padding()
@@ -64,7 +67,7 @@ struct Signup: View {
         .frame(width: 150, height: 50)
         .background(.blue)
         .foregroundColor(.black)
-        .font(.custom("American Typewriter", size: 30))
+        .font(.custom("Times New Roman", size: 30))
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .padding()
     }
