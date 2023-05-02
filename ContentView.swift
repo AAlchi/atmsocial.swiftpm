b50786b48fcb101f0e11586b90c5862e8c0e8e2a
Ali Ibrahim <alialchi07@gmail.com>
2023-05-02 14:18:26 -0500

    THIS IS A FALSE ALARM COMMIT

--- ContentView.swift
+++ ContentView.swift
@@ -5,31 +5,30 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView {
-//            VStack {
-                Home()
-//                NavigationLink("Sign Up") {
-//                    ()
-//                }
-//                .frame(width: 200, height: 100)
-//                .background(.blue)
-//                .foregroundColor(.black)
-//                .font(.custom("American Typewriter", size: 25))
-//                .clipShape(RoundedRectangle(cornerRadius: 10))
-//                .padding()
-//                NavigationLink("Sign In") {
-//                    //SignIn
-//                }
-//                .frame(width: 200, height: 100)
-//                .background(.blue)
-//                .foregroundColor(.black)
-//                .font(.custom("American Typewriter", size: 25))
-//                .clipShape(RoundedRectangle(cornerRadius: 10))
-//                .padding()
-//            }
-//            .navigationTitle("ATM Social")
-//            .sheet(isPresented: $showingSignInPage) {
-//
-//            }
+            VStack {
+                NavigationLink("Sign Up") {
+                    Signup()
+                }
+                .frame(width: 200, height: 100)
+                .background(.blue)
+                .foregroundColor(.black)
+                .font(.custom("American Typewriter", size: 25))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding()
+                NavigationLink("Sign In") {
+                    //SignIn
+                }
+                .frame(width: 200, height: 100)
+                .background(.blue)
+                .foregroundColor(.black)
+                .font(.custom("American Typewriter", size: 25))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding()
+            }
+            .navigationTitle("ATM Social")
+            .sheet(isPresented: $showingSignInPage) {
+
+            }
         }
         .navigationViewStyle(.stack)
     }
