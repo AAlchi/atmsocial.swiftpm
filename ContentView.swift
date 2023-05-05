9b3e6b1790f15f8fe53444d5eb8599ae154d39f3
Thomas Niezyniecki <tmniez@comcast.net>
2023-05-05 15:48:02 -0500

    test

--- ContentView.swift
+++ ContentView.swift
@@ -1,26 +1,5 @@
 import SwiftUI
-import MongoSwift
-
-struct ContentView: View {
-    @State var showingOptionView = false
-    
-    @State var mongoClient = MongoClient
-    
-    init() {
-        
-        let stringToConnect = ""
-        
-        if let client = try? mongoClient(stringToConnect) {
-            print("Succeeded")
-        } else {
-            print("Something Failed")
-        }
-        
-    }
     
     var body: some View {
         Option()
     }
-}
-
-
