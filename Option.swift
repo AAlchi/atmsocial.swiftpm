fb6727da6183210081df31c3502d06d9162d4ba0
Thomas Niezyniecki <tmniez@comcast.net>
2023-05-10 14:58:07 -0500

    Geometry Reader

--- Option.swift
+++ Option.swift
@@ -16,25 +16,27 @@ struct Option: View {
                 VStack {
                     Image("ATMSocial")
                         .resizable()
-                        .frame(width: geometry.size.width * 0.75, height: geometry.size.width * 0.75)
-                    NavigationLink("Sign Up") {
-                        SignUp()
+                        .frame(width: geometry.size.width * 0.6, height: geometry.size.width * 0.6)
+                    VStack {
+                        NavigationLink("Sign Up") {
+                            SignUp()
+                        }
+                        .frame(width: 250, height: 100)
+                        .background(.gray)
+                        .foregroundColor(.black)
+                        .font(.custom("American Typewriter", size: 25))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding()
+                        NavigationLink("Log In") {
+                            LogIn()
+                        }
+                        .frame(width: 250, height: 100)
+                        .background(.gray)
+                        .foregroundColor(.black)
+                        .font(.custom("American Typewriter", size: 25))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding()
                     }
-                    .frame(width: 250, height: 100)
-                    .background(.gray)
-                    .foregroundColor(.black)
-                    .font(.custom("American Typewriter", size: 25))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding()
-                    NavigationLink("Log In") {
-                        LogIn()
-                    }
-                    .frame(width: 250, height: 100)
-                    .background(.gray)
-                    .foregroundColor(.black)
-                    .font(.custom("American Typewriter", size: 25))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding()
                 }
             }
             .navigationViewStyle(.stack)
