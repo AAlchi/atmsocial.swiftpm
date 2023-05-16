03d4b3e19453b9f4aaa1ff86b36549fbef3257f6
AAlchi <alialchi07@gmail.com>
2023-05-15 20:19:50 -0500

    Adding More Changes

--- ChatGuest.swift
+++ ChatGuest.swift
@@ -12,31 +12,35 @@ struct ChatGuest: View {
     
     var body: some View {
         GeometryReader { geometry in
-            VStack(alignment: .center) {
-                Text("Display Name")
-                    .font(.custom("American Typewriter", size: 25))
-                
-                VStack {
-                    TextField("Enter Display Name", text: $displayName)
-                        .frame(width: geometry.size.width * 0.95, height: geometry.size.width * 0.08)
-                        .font(.custom("American Typewriter", size: 25))
+            VStack {
+                HStack {
+                    Text("Create Profile")
+                        .font(.system(size: geometry.size.width * 0.1))
+                        .padding()
+                }
+                Divider()
+                HStack {
+                    Text("Choose a Display Name")
+                        .font(.system(size: geometry.size.width * 0.05))
+                        .padding()
+                }
+                HStack {
+                    TextField("", text: $displayName)
                         .textFieldStyle(.roundedBorder)
                         .padding()
                 }
-                .padding()
-                
-                NavigationLink(destination: ChooseChat()
-                    .navigationBarBackButtonHidden(true)
-                ) {
-                    Text("Chat")
-                        .frame(width: 150, height: 75)
-                        .background(.gray)
-                        .foregroundColor(.black)
-                        .font(.custom("American Typewriter", size: 25))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .padding()
+                HStack {
+                    Button("Start Chatting") {
+                        
+                    }
+                    .background(
+                        NavigationLink("Start Chatting", destination: {
+                            ChooseChat()
+                        })
+                        .navigationBarBackButtonHidden(true)
+                    )
+                   
                 }
-                
             }
         }
     }
--- Option.swift
+++ Option.swift
@@ -9,44 +9,39 @@ import SwiftUI
 
 struct Option: View {
     @State private var isShowingOptionView = true
-    
+    @State private var popoverPre = false
     var body: some View {
         GeometryReader { geometry in
             NavigationView {
                 VStack {
-                    Image("ATMSocial")
-                        .resizable()
-                        .frame(width: geometry.size.width * 0.6, height: geometry.size.width * 0.6)
-                        .padding()
-                    NavigationLink("Chat") {
-                        ChatGuest()
+                    HStack {
+                        Image("ATMSocial")
+                            .resizable()
+                            .frame(width: geometry.size.width * 0.15, height: geometry.size.width * 0.15)
+                            .padding()
+                            .cornerRadius(50)
+                        Text("ATM Social").font(.system(size: 20))
                     }
-                    .frame(width: 250, height: 100)
+                   
+                    Button(action: {
+                        popoverPre = true
+                    }, label: {
+                        Text("Chat Now")
+                    })
+                    .frame(width: geometry.size.width * 0.40, height: 100)
                     .background(.gray)
                     .foregroundColor(.black)
                     .font(.custom("American Typewriter", size: 25))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .padding()
-//                    VStack {
-//                        NavigationLink("Sign Up") {
-//                            SignUp()
-//                        }
-//                        .frame(width: 250, height: 100)
-//                        .background(.gray)
-//                        .foregroundColor(.black)
-//                        .font(.custom("American Typewriter", size: 25))
-//                        .clipShape(RoundedRectangle(cornerRadius: 10))
-//                        .padding()
-//                        NavigationLink("Log In") {
-//                            LogIn()
-//                        }
-//                        .frame(width: 250, height: 100)
-//                        .background(.gray)
-//                        .foregroundColor(.black)
-//                        .font(.custom("American Typewriter", size: 25))
-//                        .clipShape(RoundedRectangle(cornerRadius: 10))
-//                        .padding()
-//                    }
+                    .foregroundColor(Color.white)
+                    .popover(isPresented: $popoverPre) {
+                        ChatGuest()
+                        Button("Cancel") {
+                            popoverPre = false
+                        }
+                    }
+                     
                 }
             }
             .navigationViewStyle(.stack)
